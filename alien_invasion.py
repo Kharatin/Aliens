3178df1b1d5cac786b48c08e991ac18b6ae1ee24
Maximus <Kharatin85@gmail.com>
2024-12-17 17:54:09 +0200

    add window

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,4 +2,29 @@ import sys
 
 import  pygame
 
+class AlienInvasion:
+    """ Общий класс который упровляет ресурсами и поведением игры. """
+
+    def __init__(self):
+       """ Инициализация игры, создать ресурсы игры. """
+       pygame.init()
+
+       self.screen = pygame.display.set_mode((1200, 800))
+       pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """ Начять главный цикл игры. """
+        while True:
+            # следить за действиями мыши и клавиатуры.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # показать последний нарисованный экран
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # создать экземпляр игры и запустить её.
+    ai = AlienInvasion()
+    ai.run_game()
 
---
+++ p.py
@@ -0,0 +1,10 @@
+
+import sys
+
+import pygame
+
+
+pygame.init()
+pygame.display.set_mode((600,600))
+while 1 :
+    pass
